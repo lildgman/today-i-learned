2d457e2444a1d1680b8d59b82dc26c572cd5989a
lildgman <humanorc2357@gmail.com>
2024-10-08 12:58:01 +0900

    241008 이코테 - 이진탐색 1문제

--- coding-test/src/binarysearch/dongbin/BinarySearch_Loop.java
+++ coding-test/src/binarysearch/dongbin/BinarySearch_Loop.java
@@ -1,5 +1,6 @@
 package binarysearch.dongbin;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,47 +9,50 @@ import java.util.StringTokenizer;
 public class BinarySearch_Loop {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 원소의 개수
-        int n = Integer.parseInt(br.readLine());
-        // 찾는 원소
-        int target = Integer.parseInt(br.readLine());
 
-        // 전체 원소 입력 받기
-        int[] arr = new int[n];
+        System.out.println("원소의 개수와 찾고하자 하는 값을 입력하세요. 구분은 띄어쓰기로 합니다.");
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+
+        // 원소 개수
+        int n = Integer.parseInt(st.nextToken());
+
+        // 찾고자 하는 문자열
+        int target = Integer.parseInt(st.nextToken());
+
+        // 원소의 값들 입력받기
+        System.out.println("입력한 원소의 개수만큼 문자열을 입력해주세요. 구분은 띄어쓰기로 합니다.");
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         // 이진 탐색 수행 결과 출력
-        int result = binarySearchLoop(arr, target, 0, n - 1);
+        int result = binarySearch(arr, target, 0, n - 1);
         if (result == -1) {
-            System.out.println("해당 원소를 찾지 못했습니다.");
+            System.out.println("찾고자 하는 값이 존재하지 않습니다.");
         } else {
             System.out.println(result + 1);
         }
     }
 
-    private static int binarySearchLoop(int[] arr, int target, int start, int end) {
+    private static int binarySearch(int[] arr, int target, int start, int end) {
         while (start <= end) {
-
             int mid = (start + end) / 2;
-
             // 찾은 경우 중간점 인덱스 반환
             if (arr[mid] == target) {
                 return mid;
             } else if (arr[mid] > target) {
-                // 중간점의 값보다 타겟의 값이 작은 경우 왼쪽 확인
+                // 중간점의 값보다 타겟의 값이 작을 경우 왼쪽부분 확인
                 end = mid - 1;
             } else {
-                // 중간점의 값보다 타겟의 값이 더 큰 경우 오른쪽 확인
+                // 중간점의 값보다 타겟의 값이 큰 경우 오른쪽 부분 확인
                 start = mid + 1;
             }
-
         }
-
         return -1;
     }
 }
--- coding-test/src/binarysearch/dongbin/BinarySearch_Recursion_Ex1.java
+++ coding-test/src/binarysearch/dongbin/BinarySearch_Recursion_Ex1.java
@@ -1,58 +1,61 @@
 package binarysearch.dongbin;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class BinarySearch_Recursion_Ex1 {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("원소의 개수와 찾고자 하는 값을 입력해주세요. 구분은 띄어쓰기로 합니다.");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         // 원소의 개수
-        System.out.println("원소의 개수를 입력하세요");
-        int n = Integer.parseInt(br.readLine());
-        // 찾고자 하는 값
-        System.out.println("찾고자 하는 값을 입력해주세요");
-        int target = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
 
-        // 전체 원소 입력 받기
-        int[] arr = new int[n];
-        System.out.println("전체 원소들을 입력해주세요. 띄어쓰기 한 칸으로 구분해주세요");
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        // 타켓
+        int target = Integer.parseInt(st.nextToken());
+
+        // 전체 원소 입력받기
+        System.out.println("정렬된 원소를 입력해주세요. 구분은 띄어쓰기로 합니다.");
+        st = new StringTokenizer(br.readLine());
+
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 이진 탐색 수행
+        // 이진 탐색 시작
         int result = binarySearch(arr, target, 0, n - 1);
         if (result == -1) {
-            System.out.println("해당 원소가 존재하지 않습니다.");
+            System.out.println("값이 존재하지 않습니다.");
         } else {
             System.out.println(result + 1);
         }
-
     }
 
-    // 재귀함수를 이용한 이진탐색
-    private static int binarySearch(int[] arr, int target, int start, int end) {
-
+    private static int binarySearch(Integer[] arr, int target, int start, int end) {
         if (start > end) {
             return -1;
         }
 
-        // 중간점
+        // 중간 인덱스
         int mid = (start + end) / 2;
-        // 찾은 경우 중간점 인덱스를 반환
+
         if (arr[mid] == target) {
             return mid;
         } else if (arr[mid] > target) {
-            // 중간점의 값보다 타켓의 값이 더 작은 경우
+            // 찾고자하는 값이 중간 인덱스의 값보다 작을 경우
             return binarySearch(arr, target, start, mid - 1);
         } else {
-            // 중간점의 값보다 타겟의 값이 더 큰 경우
+            // 찾고자하는 값이 중간 인덱스의 값도가 클 경우
             return binarySearch(arr, target, mid + 1, end);
         }
+
+
+
     }
 }
