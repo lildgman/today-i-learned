f44f256244e59dcc4243cb8bce55d4da6fa41f8d
lildgman <humanorc2357@gmail.com>
2024-10-17 15:03:48 +0900

    241017 이코테 dp, 1로 만들기, 프로그래머스 입문 및 기초

--- coding-test/src/dp/dongbin/Ex2.java
+++ coding-test/src/dp/dongbin/Ex2.java
@@ -1,4 +1,6 @@
 package dp.dongbin;
+import java.io.*;
+import java.util.*;
 
 public class Ex2 {
 
@@ -9,11 +11,45 @@ public class Ex2 {
      * 3. X가 2로 나누어 떨어지면 2로 나눈다.
      * 4. X에서 1을 뺀다
      */
-
     // 연산 4개를 사용하여 X를 1로 만들고자 한다. 연산을 사용하는 횟수의 최솟값을 출력해라
 
     // 입력 조건
     // 첫째 줄에 정수 X가 주어진다.
     // 출력 조건
     // 첫째 줄에 연산을 하는 횟수의 최솟값을 출력한다.
+
+    // 연산 횟수를 담기 위한 dp 테이블
+    public static int[] d = new int[30001];
+
+    public static void main(String[] args) throws IOException {
+
+        // 입력을 받기 위한 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 정수 x
+        int x = Integer.parseInt(br.readLine());
+
+        // DP 진행
+        // d[0]과 d[1]일 때는 연산 횟수가 없으므로 for문을 돌 필요가 없음
+        for (int i = 2; i <= x; i++) {
+
+            // 현재 수에서 1을 빼는 경우
+            d[i] = d[i - 1] + 1;
+
+            // 현재 수가 2로 나누어 떨어지는 경우
+            if (i % 2 == 0) {
+                d[i] = Math.min(d[i], d[i / 2] + 1);
+            }
+            // 현재 수가 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0) {
+                d[i] = Math.min(d[i], d[i / 3] + 1);
+            }
+            // 현재 수가 5로 나누어 떨어지는 경우
+            if (i % 5 == 0) {
+                d[i] = Math.min(d[i], d[i / 5] + 1);
+            }
+        }
+
+        System.out.println(d[x]);
+    }
 }
